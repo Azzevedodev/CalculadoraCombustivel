ed02aeff26893be54dfd8d6446af1df17e7e5a6a
Hugo pc <azzevedohugodev@gmail.com>
2025-02-15 09:54:11 -0300

    - Criação e finalização da DistanciaActivity

--- app/src/main/java/com/example/calculadoradecombustivel/DistanciaActivity.kt
+++ app/src/main/java/com/example/calculadoradecombustivel/DistanciaActivity.kt
@@ -1,20 +1,37 @@
 package com.example.calculadoradecombustivel
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class DistanciaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_distancia)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnDistancia = findViewById<TextInputEditText>(R.id.edt_distancia)
+        val btnProximo3 = findViewById<Button>(R.id.btn_proximo3)
+        btnProximo3.setOnClickListener {
+
+            val distanciaStr: String = btnDistancia.text.toString()
+            if (distanciaStr.isEmpty()) {
+                Snackbar.make(
+                    btnDistancia,
+                    "Preencha o campo vazio para continuar",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                val distancia = distanciaStr.toFloat()
+                val intent = Intent(this, ResultActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
