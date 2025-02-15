20c53eba7f9c3f0f8192093b4eb05a4d16518372
Hugo pc <azzevedohugodev@gmail.com>
2025-02-14 22:46:14 -0300

    Mensagem na tela: Preencha o campo vazio para continuar

---
+++ app/src/main/java/com/example/calculadoradecombustivel/ConsumoActivity.kt
@@ -0,0 +1,38 @@
+package com.example.calculadoradecombustivel
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class ConsumoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_consumo)
+
+        val btnConsumo = findViewById<TextInputEditText>(R.id.edt_consumo)
+        val btnProximo2 = findViewById<Button>(R.id.btn_proximo2)
+        btnProximo2.setOnClickListener {
+
+            val consumoStr: String = btnConsumo.text.toString()
+            if (consumoStr.isEmpty()) {
+                Snackbar.make(
+                    btnConsumo,
+                    "Preencha o campo vazio para continuar",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                val consumo = consumoStr.toFloat()
+                val intent = Intent(this, DistanciaActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+}
